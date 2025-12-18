273f2973291bcfec1c967f5fd75af1823cfa874b
sirjofri <sirjofri@sirjofri.de>
2025-12-18 19:26:04 +0100

    don't move generated pngs, adds team preset (might need to adjust color a bit)

--- export.bat
+++ export.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 
 for /f "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\inkscape.exe" /v "Path"') do set inkscape=%%b
 
@@ -6,6 +6,3 @@ for %%f in (*.svg) do (
 "%inkscape%\inkscape.exe" %%f --export-type=png --export-page=all
 echo %%f
 )
-
-mkdir output 2>NUL
-move *.png output 2>NUL
