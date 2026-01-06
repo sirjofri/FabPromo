a5829fcbfe60f52298e9af2b6f1f6c777ba1e8f0
sirjofri <sirjofri@sirjofri.de>
2026-01-06 13:40:58 +0100

    adds default fill to font, adjust export script for multiple files

--- export.bat
+++ export.bat
@@ -3,6 +3,6 @@
 for /f "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\inkscape.exe" /v "Path"') do set inkscape=%%b
 
 for %%f in (*.svg) do (
-"%inkscape%\inkscape.exe" %%f --export-type=png --export-page=all
+"%inkscape%\inkscape.exe" %%f --export-type=png
 echo %%f
 )
